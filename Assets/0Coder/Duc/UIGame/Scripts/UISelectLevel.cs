9853a1237e77e1d2584651435ad32e7c0a4a5a33
tranduc0102 <tranhuynhduc.vn@gmail.com>
2025-03-28 20:04:44 +0700

    Finish UI Game
    Finish UI Game

---
+++ Assets/0Coder/Duc/UIGame/Scripts/UISelectLevel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UIGame
+{
+    public class UISelectLevel : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _canvasGroup;
+        public void ShowDisplay(bool enable, UnityAction onShow = null, UnityAction onClosed = null)
+        {
+            if (enable)
+            {
+                onShow?.Invoke();
+                _canvasGroup.gameObject.SetActive(true);
+                _canvasGroup.DOFade(1, 0.5f);
+            }
+            else
+            {
+                _canvasGroup.DOFade(0, 0.5f).OnComplete(() =>
+                {
+                    _canvasGroup.gameObject.SetActive(false);
+                    onClosed?.Invoke();
+                });
+            }
+        }
+    }
+
+}
---
+++ Assets/0Coder/Duc/UIGame/Scripts/UISetting.cs
@@ -0,0 +1,81 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace UIGame
+{
+    public class UISetting : BasePopup
+    {
+        [Header("Slider Sound FX")]
+        [SerializeField] private Slider _sliderSoundFX;
+        [SerializeField] private Image iconSound;
+        public Sprite iconOnSoundFX;
+        public Sprite iconOffSoundFX;
+        [Header("Slider Music")]
+        [SerializeField] private Slider _sliderMusic;
+        [SerializeField] private Image iconMusic;
+        public Sprite iconOnMusic;
+        public Sprite iconOffMusic;
+        private UnityAction _actionClosed;
+
+        private void Start()
+        {
+            
+            if (_sliderSoundFX.value == 0)
+            {
+                iconSound.sprite = iconOffSoundFX;
+            }
+            else
+            {
+                iconSound.sprite = iconOnSoundFX;
+            }
+            
+            
+            if (_sliderMusic.value == 0)
+            {
+                iconMusic.sprite = iconOffMusic;
+            }
+            else
+            {
+                iconMusic.sprite = iconOnMusic;
+            }
+            _sliderSoundFX.onValueChanged.AddListener(delegate
+            {
+                if (_sliderSoundFX.value == 0)
+                {
+                    iconSound.sprite = iconOffSoundFX;
+                }
+
+                if (iconSound.sprite == iconOffSoundFX && _sliderSoundFX.value != 0)
+                {
+                    iconSound.sprite = iconOnSoundFX;
+                }
+            });
+            
+            _sliderMusic.onValueChanged.AddListener(delegate
+            {
+                if (_sliderMusic.value == 0)
+                {
+                    iconMusic.sprite = iconOffMusic;
+                }
+
+                if (iconMusic.sprite == iconOffMusic && _sliderMusic.value != 0)
+                {
+                    iconMusic.sprite = iconOnMusic;
+                }
+            });
+            
+        }
+
+        public void SetActionClosed(UnityAction action)
+        {
+            _actionClosed = action;
+        }
+        public void Close()
+        {
+            ShowDisplay(false, null, _actionClosed);
+        }
+        
+    }
+}
