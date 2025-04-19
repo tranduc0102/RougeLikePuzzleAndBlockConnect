9c08f6a1740abaf63f1661822283c317cd9fef34
Vo_Duyen <125801734+Vo-Duyen@users.noreply.github.com>
2025-04-19 10:47:54 +0700

    Update State Selecting and Previewing

---
+++ Assets/0Coder/Long/!/Scripts/Scene_-1_Test/hi.cs
@@ -0,0 +1,17 @@
+// Author: Dan_lang_A (DauHang)
+
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DanLangA
+{
+    public class hi : MonoBehaviour
+    {
+        private void Update()
+        {
+            // transform.position
+        }
+    }
+}
